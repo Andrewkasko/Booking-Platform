69c053a17ecf219c27067a76494ff3d39822af07
treevesy <63341855+treevesy@users.noreply.github.com>
2020-10-07 22:58:43 +1100

    Add files via upload

--- app.js
+++ app.js
@@ -24,6 +24,21 @@ mongoose.connect(db, {useNewUrlParser:true, useUnifiedTopology: true })
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+//Print addresses in database
+/*var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost/TestDB';*/
+
+/*mongoose.connect(url, function(err, db) {
+
+    var adresses = db.collection('doctors_location').find();
+
+    adresses.each(function(err, doc) {
+
+        console.log(doc);
+
+    });
+});*/
+
 // Bodyparser
 app.use(express.urlencoded({ extended: false }));
 
@@ -54,13 +69,25 @@ app.use((req, res, next) => {
 //Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
-
+var addressRouter = require('./routes/distance');
+app.use('/map', addressRouter);
+//router.get('/data-list',addressController.fetchData);
+//app.use('/doctors_location', require('./routes/distance'));
 
 
 // DASHBOARD
 app.use('/dashboard', ensureAuthenticated);
 app.use(ensureAuthenticated, express.static(__dirname + '/views/dashboard'));
 ////////////
+
+// get the doctors address's from the database
+/*app.get('/doctors_location_get', (req, res) => {
+
+    db.collection('doctors_location').find().toArray((err, result) => {
+        if (err) return console.log(err);
+        res.send(result);
+    });
+});*/
 
 const PORT = process.env.PORT || 5000;
 
